d3c262a553e6d82bb1cdf7232f8b009772b49663
leiber <leiberbertel777@gmail.com>
2023-09-23 22:58:42 -0500

    step-4 and step-5: event and property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  name = 'leiber';
-  age = 21;
+  name: string = 'leiber';
+  age: number = 21;
   img = 'https://source.unsplash.com/random';
+  // property-binding
+  btnDisable: boolean = false;
+  nameValue: string = "Insertar info";
+
+  person = {
+    name: "Leiber",
+    age: 21,
+    img: 'https://source.unsplash.com/random'
+  }
 }
